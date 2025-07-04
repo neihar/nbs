803ae8de583dde946afcd4bba83ed5e54135df80
voidspace01 <voidspace01@gmail.com>
2025-07-04 12:15:12 +0200

    fix

--- cloud/filestore/libs/vfs_fuse/fs_impl_ut.cpp
+++ cloud/filestore/libs/vfs_fuse/fs_impl_ut.cpp
@@ -1,21 +1,31 @@
 #include "fs_impl.h"
+
 #include "fs.h"
 
-#include <cloud/storage/core/libs/common/scheduler_test.h>
-#include <library/cpp/testing/unittest/registar.h>
-#include <cloud/storage/core/libs/diagnostics/logging.h>
-#include <cloud/storage/core/libs/common/timer.h>
-#include <util/folder/tempdir.h>
-#include <util/folder/path.h>
+#include <cloud/filestore/libs/client/config.h>
+#include <cloud/filestore/libs/client/session.h>
 #include <cloud/filestore/libs/diagnostics/profile_log.h>
 #include <cloud/filestore/libs/diagnostics/public.h>
 #include <cloud/filestore/libs/service/filestore_test.h>
-#include <cloud/filestore/libs/client/session.h>
-#include <cloud/filestore/libs/client/config.h>
+
+#include <cloud/storage/core/libs/common/scheduler_test.h>
+#include <cloud/storage/core/libs/common/timer.h>
+#include <cloud/storage/core/libs/diagnostics/logging.h>
+
+#include <library/cpp/testing/unittest/registar.h>
+
+#include <util/folder/path.h>
+#include <util/folder/tempdir.h>
+#include <library/cpp/monlib/dynamic_counters/counters.h>
+#include <cloud/storage/core/libs/user_stats/counter/user_counter.h>
 
 namespace NCloud::NFileStore::NFuse {
 
 namespace {
+
+using namespace NCloud::NStorage::NUserStats;
+using namespace NMonitoring;
+using namespace NCloud;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -23,52 +33,58 @@ class TBootstrap
 {
 public:
     TBootstrap()
-        : Logging(CreateLoggingService(
-              "console",
-              {.FiltrationLevel = TLOG_RESOURCES}))
-        , ProfileLog(CreateProfileLogStub())
-        , Scheduler(new TTestScheduler())
-        , Timer(CreateWallClockTimer())
-        , Stats(nullptr)
-        , Queue(nullptr)
-
     {
+        auto logFs= CreateLoggingService("fs");
+        NCloud::NFileStore::IProfileLogPtr profileLogFs(CreateProfileLogStub());
+        ISchedulerPtr schedulerFs(new TTestScheduler());
+        ITimerPtr timerFs(CreateWallClockTimer());
+
         NProto::TFileSystemConfig protoConfig;
-        protoConfig.SetFileSystemId("tst");
-        Config = std::make_shared<TFileSystemConfig>(protoConfig);
+        protoConfig.SetFileSystemId("tst_fs_id");
+        TFileSystemConfigPtr configFs = std::make_shared<TFileSystemConfig>(protoConfig);
 
-        Service = std::make_shared<TFileStoreTest>();
-
+        auto logSession= CreateLoggingService("session");
+        ITimerPtr timerSession(CreateWallClockTimer());
+        ISchedulerPtr schedulerSession(new TTestScheduler());
+        auto serviceSession = std::make_shared<TFileStoreTest>();
         auto sessionConfig =
             std::make_shared<NCloud::NFileStore::NClient::TSessionConfig>(
                 NProto::TSessionConfig{});
-
-        Session = NCloud::NFileStore::NClient::CreateSession(
-            Logging,
-            Timer,
-            Scheduler,
-            Service,
+        IFileStorePtr sessionFs = NCloud::NFileStore::NClient::CreateSession(
+            logSession,
+            timerSession,
+            schedulerSession,
+            serviceSession,
             sessionConfig);
 
-        THandleOpsQueuePtr handleOpsQueue(CreateHandleOpsQueue("tst", 1));
-        TWriteBackCachePtr writeBackCache(CreateWriteBackCache("tst", 1));
+    TDynamicCountersPtr countersStats = MakeIntrusive<TDynamicCounters>();
+    ITimerPtr timerStats = CreateWallClockTimer();
+    IRequestStatsRegistryPtr registryStats = CreateRequestStatsRegistryStub();
 
-        FileSystem = CreateFileSystem(
-            Logging,
-            ProfileLog,
-            Scheduler,
-            Timer,
-            Config,
-            Session,
-            Stats,
-            Queue,
-            std::move(handleOpsQueue),
-            std::move(writeBackCache));
+        IRequestStatsPtr statsFs = registryStats->GetFileSystemStats(
+            configFs->GetFileSystemId(),
+            "tst_client");
+
+        //ICompletionQueuePtr Queue;
+
+//        THandleOpsQueuePtr handleOpsQueue(CreateHandleOpsQueue("tst2", 1));
+//        TWriteBackCachePtr writeBackCache(CreateWriteBackCache("tst3", 1));
+
+        //        FileSystem = CreateFileSystem(
+        //            logFs,
+        //            profileLogFs,
+        //            schedulerFs,
+        //            timerFs,
+        //            configFs,
+        //            sessionFs,
+        //            Stats,
+        //            Queue,
+        //            std::move(handleOpsQueue),
+        //            std::move(writeBackCache));
     }
 
     ~TBootstrap()
-    {
-    }
+    {}
 
     IFileSystemPtr GetFs()
     {
@@ -76,16 +92,6 @@ public:
     }
 
 private:
-
-    ILoggingServicePtr Logging;
-    NCloud::NFileStore::IProfileLogPtr ProfileLog;
-    ISchedulerPtr Scheduler;
-    ITimerPtr Timer;
-    TFileSystemConfigPtr Config;
-    IFileStorePtr Session;
-    IRequestStatsPtr Stats;
-    ICompletionQueuePtr Queue;
-
     std::shared_ptr<TFileStoreTest> Service;
 
     IFileSystemPtr FileSystem;
